5b6dd64219d5f1a2b8be6d089550da31f8577a86
Atsushi Shimono <atsushi@himor.in>
2018-01-05 20:31:07 +0900

    Dealt with white-space in filename and added exts

--- gitlfs.sh
+++ gitlfs.sh
@@ -1,18 +1,30 @@
 #! /bin/sh
 
 git filter-branch --prune-empty --tree-filter '
-git lfs track "*.fits"
-git lfs track "*.pck"
-git lfs track "*.imgstk"
-git lfs track "*.dat"
-git lfs track "*.gz"
-git add .gitattributes .gitconfig
+  git lfs track "*.png"
+  git lfs track "*.PNG"
+  git lfs track "*.jpg"
+  git lfs track "*.JPG"
+  git lfs track "*.jpeg"
+  git lfs track "*.JPEG"
+  git lfs track "*.vsd"
+  git lfs track "*.pdf"
+  git lfs track "*.ods"
+  git lfs track "*.doc"
+  git lfs track "*.docx"
+  git lfs track "*.eps"
+  git lfs track "*.ppt"
+  git lfs track "*.pptx"
+  git lfs track "*.mpp"
+  git add .gitattributes 
 
-for file in $(git ls-files | xargs git check-attr filter | grep "filter: lfs" | sed -r "s/(.*): filter: lfs/\1/"); do
-  echo "Processing ${file}"
+  for file in $(git ls-files | xargs -i git check-attr filter "{}" | grep "filter: lfs" | sed -r "s/^(.*): filter: lfs/\1/" | sed -e "s/ /\%/g"); do
+    echo "OPT: ${file}"
+    file=`echo ${file} | sed -e "s/\%/ /g"`
+    echo "Processing ${file}"
+    git rm -f --cached "${file}"
+    echo "Adding $file lfs style"
+    git add "${file}"
+  done' --tag-name-filter cat -- --all
 
-  git rm -f --cached ${file}
-  echo "Adding $file lfs style"
-  git add ${file}
-done' --tag-name-filter cat -- --all
 
